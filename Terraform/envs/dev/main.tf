b53f0c444b1cd1a1d7ad5508427109a96c488314
Prashun Chakraborty <prashun.ch@gmail.com>
2026-02-12 00:00:08 +0530

    Refactor Terraform configuration for improved readability and add .terraform.lock.hcl file

--- Terraform/envs/dev/main.tf
+++ Terraform/envs/dev/main.tf
@@ -7,8 +7,8 @@ module "network" {
   source              = "../../modules/network"
   resource_group_name = azurerm_resource_group.this.name
   location            = var.location
-  vnet_cidr            = var.vnet_cidr
-  subnet_cidr          = var.subnet_cidr
+  vnet_cidr           = var.vnet_cidr
+  subnet_cidr         = var.subnet_cidr
 }
 
 module "aks" {
